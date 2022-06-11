f45fe7300ad48612436e0c2151eff207fce3572f
RdotSilva <RyandotSilva@gmail.com>
2022-06-11 17:56:45 +0100

    Replace id with itemId

--- lok_marketplace/potions/models.py
+++ lok_marketplace/potions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Potion(models.Model):
     category = models.CharField(max_length=200)
-    id = models.CharField(max_length=200)
+    itemId = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
