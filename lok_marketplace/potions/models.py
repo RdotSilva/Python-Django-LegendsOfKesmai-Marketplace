41703e1d91544c58e1acbd1d582c1b6009a17797
RdotSilva <RyandotSilva@gmail.com>
2022-06-12 18:59:28 +0100

    Add __str__ method

--- lok_marketplace/potions/models.py
+++ lok_marketplace/potions/models.py
@@ -7,3 +7,6 @@ class Potion(models.Model):
     image = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
