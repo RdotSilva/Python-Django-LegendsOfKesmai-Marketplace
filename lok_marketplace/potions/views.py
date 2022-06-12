8d4941c54ecc57a7fbbda1a1e4999b9a57516ccb
RdotSilva <RyandotSilva@gmail.com>
2022-06-12 19:17:10 +0100

    Add getPotions function

--- lok_marketplace/potions/views.py
+++ lok_marketplace/potions/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the potions index.")
+@api_view(["GET"])
+def getPotions(request):
+    potions = [
+        {
+            "name": "Strength Potion",
+            "image": "item-00225.png",
+            "slug": "strengthpotion",
+            "itemId": "00225",
+            "category": "potions",
+        },
+        {
+            "name": "Dexterity Potion",
+            "image": "item-00222.png",
+            "slug": "dexteritypotion",
+            "itemId": "00222",
+            "category": "potions",
+        },
+    ]
+    return Response(potions)
