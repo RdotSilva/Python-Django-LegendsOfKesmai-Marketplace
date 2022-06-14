30ad01a30865220802b6ac4cd165d600c4da932c
RdotSilva <RyandotSilva@gmail.com>
2022-06-14 19:48:02 +0100

    Add addPotion post request

--- lok_marketplace/potions/views.py
+++ lok_marketplace/potions/views.py
@@ -9,3 +9,11 @@ def getPotions(request):
     potions = Potion.objects.all()
     serializer = PotionSerializer(potions, many=True)
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def addPotion(request):
+    serializer = PotionSerializer(data=request.data)  # Serialize data from request
+    if serializer.is_valid():  # Check to ensure data is valid
+        serializer.save()
+    return Response(serializer.data)
