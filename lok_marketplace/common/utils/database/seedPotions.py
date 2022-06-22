77169dda5ac751c8c4f80b2829e6d383a4a972a8
RdotSilva <RyandotSilva@gmail.com>
2022-06-22 19:15:54 +0100

    Update seed_potion_data logic

--- lok_marketplace/common/utils/database/seedPotions.py
+++ lok_marketplace/common/utils/database/seedPotions.py
@@ -1,13 +1,17 @@
 from django_seed import Seed
 from potions.models import Potion
+from potion_data import potion_list
 
 seeder = Seed.seeder()
 
 
 def seed_potion_data():
-    # TODO: Seed potion data to DB
-    # Loop through potion seed data and add each entity
-    seeder.add_entity(Potion, potionData)
+    for potion in potion_list:
+        seeder.add_entity(Potion, potion)
 
     # Execute the seeding
     inserted_pks = seeder.execute()
+    print(f"{inserted_pks} has been seeded")
+
+
+# TODO: Call seed_potion_data and test seeding
