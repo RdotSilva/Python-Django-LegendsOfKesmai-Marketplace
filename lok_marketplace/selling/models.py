16d1f2088cd891acc92f5f0aac1c059586b288bf
RdotSilva <RyandotSilva@gmail.com>
2022-06-28 18:11:20 +0100

    Add Selling model

--- lok_marketplace/selling/models.py
+++ lok_marketplace/selling/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Selling(models.Model):
+    category = models.CharField(max_length=200)
+    itemId = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    user = models.CharField(max_length=200)
+    price = models.IntegerField(max_length=9)
+    quantity = models.IntegerField(max_length=3)
+
+    def __str__(self):
+        return self.name
