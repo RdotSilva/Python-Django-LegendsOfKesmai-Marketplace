a70499d2a4ef061106d9d5d6bd243e163e67db12
RdotSilva <RyandotSilva@gmail.com>
2022-06-30 19:20:21 +0100

    Add TODO for SellingView

--- lok_marketplace/selling/views.py
+++ lok_marketplace/selling/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from selling.models import Selling
+from .serializers import SellingSerializer
 
-# Create your views here.
+# TODO: Create SellingView class
